3f6cd7faa37729f0c300d4fa1b1c70cd2c884ba3
nicolasgandrade <nicolasguerrero72@gmail.com>
2024-05-16 16:31:02 -0300

    feat: move the header to a separate component

--- src/app/core/layouts/workspace/containers/root-workspace/root-workspace.component.ts
+++ src/app/core/layouts/workspace/containers/root-workspace/root-workspace.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { SidebarComponent } from '../../components/sidebar/sidebar.component';
+import { HeaderComponent } from '../../components/header/header.component';
 
 @Component({
   selector: 'app-root-workspace',
   standalone: true,
-  imports: [SidebarComponent],
+  imports: [SidebarComponent, HeaderComponent],
   templateUrl: './root-workspace.component.html',
   styleUrl: './root-workspace.component.scss',
 })
