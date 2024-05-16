9b8f6591e271a2fe6c9ab87f1c86e19fa51692c3
nicolasgandrade <nicolasguerrero72@gmail.com>
2024-05-16 18:20:21 -0300

    feat: create error state component

---
+++ src/app/shared/components/error-state/error-state.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-error-state',
+  standalone: true,
+  imports: [],
+  templateUrl: './error-state.component.html',
+  styleUrl: './error-state.component.scss'
+})
+export class ErrorStateComponent {
+
+}
