32306d35cc03cb4ba4bebac2e381998405423c69
nicolasgandrade <nicolasguerrero72@gmail.com>
2024-05-17 00:01:15 -0300

    test: fix breaking test due to missing dependency on root-episodes

--- src/app/features/episodes/containers/root-episodes/root-episodes.component.spec.ts
+++ src/app/features/episodes/containers/root-episodes/root-episodes.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RootEpisodesComponent } from './root-episodes.component';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('RootEpisodesComponent', () => {
   let component: RootEpisodesComponent;
@@ -8,10 +9,9 @@ describe('RootEpisodesComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [RootEpisodesComponent]
-    })
-    .compileComponents();
-    
+      imports: [RootEpisodesComponent, HttpClientModule],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(RootEpisodesComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
