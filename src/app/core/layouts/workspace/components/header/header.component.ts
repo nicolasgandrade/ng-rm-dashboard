5e2ce2013630bf745f3879c179d8066ccf512789
nicolasgandrade <nicolasguerrero72@gmail.com>
2024-05-18 16:28:24 -0300

    feat: create a global search service

--- src/app/core/layouts/workspace/components/header/header.component.ts
+++ src/app/core/layouts/workspace/components/header/header.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input, input } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
-
+  @Input({ required: true }) searchControl!: FormControl<string | null>;
 }
--- src/app/features/episodes/containers/root-episodes/root-episodes.component.ts
+++ src/app/features/episodes/containers/root-episodes/root-episodes.component.ts
@@ -1,12 +1,12 @@
-import { Component, OnInit, Signal, inject } from '@angular/core';
+import { Component, OnInit, Signal, effect, inject } from '@angular/core';
 import { ListComponent } from '../../../../shared/components/list/list.component';
 import { EpisodesStore } from '../../state/episodes.store';
 import { EpisodesService } from '../../services/episodes.service';
-import { HttpClientModule } from '@angular/common/http';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { EpisodeCardComponent } from '../../components/episode-card/episode-card.component';
 import { InfiniteScrollModule } from 'ngx-infinite-scroll';
 import { LoaderComponent } from '../../../../shared/components/loader/loader.component';
+import { GlobalSearchService } from '../../../../shared/services/global-search.service';
 
 @Component({
   selector: 'app-root-episodes',
@@ -17,16 +17,21 @@ import { LoaderComponent } from '../../../../shared/components/loader/loader.com
     LoaderComponent,
     InfiniteScrollModule,
   ],
-  providers: [EpisodesStore, EpisodesService, HttpClientModule],
+  providers: [EpisodesStore, EpisodesService],
   templateUrl: './root-episodes.component.html',
   styleUrl: './root-episodes.component.scss',
 })
 export class RootEpisodesComponent implements OnInit {
   private readonly episodesStore = inject(EpisodesStore);
+  private readonly globalSearchService = inject(GlobalSearchService);
 
   readonly isLoading = toSignal(this.episodesStore.isLoading$);
   readonly hasError = toSignal(this.episodesStore.hasError$);
   readonly listItems = toSignal(this.episodesStore.listItems$);
+
+  constructor() {
+    effect(() => console.log(this.globalSearchService.searchTerm()));
+  }
 
   ngOnInit(): void {
     this.fetchEpisodes();
