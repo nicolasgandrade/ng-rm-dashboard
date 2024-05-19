b547a8c48ea0295d09780a0d6070c319552ee4fe
nicolasgandrade <nicolasguerrero72@gmail.com>
2024-05-18 21:39:49 -0300

    feat: make the sidebar responsive

--- src/app/core/layouts/workspace/components/sidebar/sidebar.component.ts
+++ src/app/core/layouts/workspace/components/sidebar/sidebar.component.ts
@@ -1,14 +1,31 @@
-import { Component, output } from '@angular/core';
+import { Component, HostListener, OnInit, output, signal } from '@angular/core';
 import { sidebarConfig } from '../../utils/sidebar-config';
-
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-sidebar',
   standalone: true,
+  imports: [CommonModule],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss',
 })
-export class SidebarComponent {
+export class SidebarComponent implements OnInit {
   menuItemClicked = output<string>();
 
+  readonly isCollapsed = signal(false);
+
   readonly sidebarConfig = sidebarConfig;
+  private readonly minimumScreenSize = 767;
+
+  ngOnInit(): void {
+    this.onResize();
+  }
+
+  @HostListener('window:resize')
+  onResize() {
+    if (window.innerWidth <= this.minimumScreenSize) {
+      this.isCollapsed.set(true);
+    } else {
+      this.isCollapsed.set(false);
+    }
+  }
 }
